71e8d7b194ed1b6e5e7f6c2c9b302a7f3d353fde
playstrongph <playstrong.ph@gmail.com>
2021-04-19 07:42:18 +0800

    Serializing Interfaces

---
+++ Playstrong/Assets/Editor/RequireInterfaceAttribute.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Editor
+{
+    /// <summary>
+    /// Attribute that require implementation of the provided interface.
+    /// </summary>
+    public class RequireInterfaceAttribute : PropertyAttribute
+    {
+        // Interface type.
+        public System.Type requiredType { get; private set; }
+
+        /// <summary>
+        /// Requiring implementation of the <see cref="T:Assets.Editor.RequireInterfaceAttribute"/> interface.
+        /// </summary>
+        /// <param name="type">Interface type.</param>
+        public RequireInterfaceAttribute(System.Type type)
+        {
+            this.requiredType = type;
+        }
+    }
+}
