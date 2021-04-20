1a51e28a04011f57357c69b6e73e089535964ec7
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-04-21 06:38:00 +0800

    Load Hero Visuals WIP

---
+++ Playstrong/Assets/Scripts/Visual/LoadHeroVisuals.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Interfaces;
+using UnityEngine;
+using Utilities;
+using Object = UnityEngine.Object;
+
+public class LoadHeroVisuals : MonoBehaviour
+{
+    [SerializeReference]
+    private IHeroAsset _heroAsset;
+
+    [SerializeReference]
+    private IHeroVisualReferences _heroVisualReferences;
+
+    private void Awake()
+    {
+        _heroVisualReferences = GetComponent<IHeroVisualReferences>();
+    }
+
+    public void SetHeroAsset(IHeroAsset heroAsset)
+    {
+        _heroAsset = heroAsset;
+    }
+
+    public void LoadHeroVisualsFromHeroAsset()
+    {
+           
+    }
+
+
+
+}
