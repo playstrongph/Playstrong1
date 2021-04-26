c1fbeb2659d0d14176e62e387b923ebaf7092333
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-04-26 14:09:02 +0800

    Prefabs Controller

---
+++ Playstrong/Assets/Scripts/Logic/Prefabs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prefabs : MonoBehaviour
+{
+   private void Awake()
+   {
+      this.gameObject.SetActive(false);
+   }
+
+   private void OnApplicationQuit()
+   {
+      this.gameObject.SetActive(true);
+   }
+}
