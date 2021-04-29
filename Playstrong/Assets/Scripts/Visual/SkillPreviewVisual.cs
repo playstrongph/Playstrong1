1e6c4bb4df356063ba47c9effbf4bd9334073a45
playstrongph <playstrong.ph@gmail.com>
2021-04-29 08:43:41 +0800

    Skill Object Prefab WIP

--- Playstrong/Assets/Scripts/Visual/SkillPreviewVisual.cs
+++ Playstrong/Assets/Scripts/Visual/SkillPreviewVisual.cs
@@ -1,13 +1,22 @@
 ﻿using Interfaces;
+using References;
 using UnityEngine;
+using Utilities;
 
 namespace Visual
 {
     public class SkillPreviewVisual : MonoBehaviour, ISkillPreviewVisual
     {
-    
-    
-    
-    
+
+        [SerializeField] [RequireInterface(typeof(ISkillObjectReferences))]
+        private Object _skillObjectReferences;
+        public ISkillObjectReferences SkillObjectReferences => _skillObjectReferences as ISkillObjectReferences;
+
+        [SerializeField] private Canvas _previewCanvas;
+        public Canvas PreviewCanvas => _previewCanvas;
+        
+        
+
+
     }
 }
