5c4477711588fb0b981458514e40cae6428681bb
playstrongph <playstrong.ph@gmail.com>
2021-05-15 08:08:14 +0800

    HeroStatus WIP

---
+++ Playstrong/Assets/Scripts/Logic/ISetHeroStatus.cs
@@ -0,0 +1,11 @@
+﻿using ScriptableObjects;
+
+namespace Logic
+{
+    public interface ISetHeroStatus
+    {
+        IHeroStatusAsset HeroActive { get; }
+        IHeroStatusAsset HeroInactive { get; }
+        IHeroStatusAsset HeroDead { get; }
+    }
+}
---
+++ Playstrong/Assets/Scripts/Logic/SetHeroStatus.cs
@@ -0,0 +1,24 @@
+﻿using ScriptableObjects;
+using UnityEngine;
+using Utilities;
+
+namespace Logic
+{
+    public class SetHeroStatus : MonoBehaviour, ISetHeroStatus
+    {
+        [SerializeField] [RequireInterface(typeof(IHeroStatusAsset))]
+        private ScriptableObject _heroActive;
+        public IHeroStatusAsset HeroActive => _heroActive as IHeroStatusAsset;
+        
+        [SerializeField] [RequireInterface(typeof(IHeroStatusAsset))]
+        private ScriptableObject _heroInactive;
+        public IHeroStatusAsset HeroInactive => _heroInactive as IHeroStatusAsset;
+        
+        [SerializeField] [RequireInterface(typeof(IHeroStatusAsset))]
+        private ScriptableObject _heroDead;
+        public IHeroStatusAsset HeroDead => _heroDead as IHeroStatusAsset;
+
+
+
+    }
+}
