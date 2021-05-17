2031ea6b2a25422b195017a4b3354bab3dcf4ab2
playstrongph <playstrong.ph@gmail.com>
2021-05-18 07:57:22 +0800

    Target  Hero WIP

---
+++ Playstrong/Assets/Scripts/Logic/HeroPreview.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using Interfaces;
+using UnityEngine;
+using Utilities;
+using Visual;
+
+namespace Logic
+{
+    public class HeroPreview : MonoBehaviour, ITargetPreview
+    {
+        
+        [SerializeField] [RequireInterface(typeof(ITargetVisual))]
+        private Object _targetVisual;
+        public ITargetVisual TargetVisual => _targetVisual as ITargetVisual;
+        
+        
+        private IHeroPreviewVisual _heroPreviewVisual;
+        private Coroutine _showPreview;
+        private float _displayDelay;
+        
+        private ITargetHero _targetHero;
+        
+        private void Awake()
+        {
+            _targetHero = GetComponent<ITargetHero>();
+            _heroPreviewVisual = _targetHero.Hero.HeroPreviewVisual;
+            _displayDelay = 0.5f;
+         
+            //prevents null reference error for _showPreview
+            _showPreview = StartCoroutine(NullAction());
+        }
+        
+        private void OnMouseDown()
+        {
+            ShowHeroPreview();
+        }
+
+        private void OnMouseUp()
+        {
+            HidePreview();
+        }
+       
+        private void ShowHeroPreview()
+        {
+            _showPreview = StartCoroutine(ShowHeroPreviewCoroutine());
+        }
+       
+        
+        public void HidePreview()
+        {
+            StopCoroutine(_showPreview);
+            _heroPreviewVisual.PreviewCanvas.gameObject.SetActive(false);  
+        }
+        
+        
+        
+        private IEnumerator ShowHeroPreviewCoroutine()
+        {
+            yield return new WaitForSeconds(_displayDelay);
+            _heroPreviewVisual.LoadHeroPreviewVisuals.UpdateHeroPreviewAttributes();
+            _heroPreviewVisual.PreviewCanvas.gameObject.SetActive(true);
+            yield return null;
+
+        }
+
+        private IEnumerator NullAction()
+        {
+            yield return null;
+        }
+        
+        
+        
+        
+        
+        
+    }
+}
--- Playstrong/Assets/Scripts/Visual/TargetHero.cs
+++ Playstrong/Assets/Scripts/Visual/TargetHero.cs
@@ -7,65 +7,12 @@ using Object = UnityEngine.Object;
 
 namespace Visual
 {
-   public class TargetHero : MonoBehaviour, ITargetPreview, ITargetHero
+   public class TargetHero : MonoBehaviour, ITargetHero
    {
       [SerializeField]
       [RequireInterface(typeof(IHero))]
       private Object _hero;
       public IHero Hero => _hero as IHero;
-      
-      [SerializeField] [RequireInterface(typeof(ITargetVisual))]
-      private Object _targetVisual;
-      public ITargetVisual TargetVisual => _targetVisual as ITargetVisual;
-
-      //Variables initialized at Awake or Start
-      private IHeroPreviewVisual _heroPreviewVisual;
-      private Coroutine _showPreview;
-      private float _displayDelay;
-     
-
-      private void Awake()
-      {
-         _heroPreviewVisual = Hero.HeroPreviewVisual;
-         _displayDelay = 0.5f;
-         
-         //prevents null reference error for _showPreview
-         _showPreview = StartCoroutine(NullAction());
-      }
-
-      private void OnMouseDown()
-      {
-         ShowHeroPreview();
-      }
-
-      private void OnMouseUp()
-      {
-         HidePreview();
-      }
-      private void ShowHeroPreview()
-      {
-         _showPreview = StartCoroutine(ShowHeroPreviewCoroutine());
-      }
-
-      public void HidePreview()
-      {
-         StopCoroutine(_showPreview);
-         _heroPreviewVisual.PreviewCanvas.gameObject.SetActive(false);  
-      }
-
-      private IEnumerator ShowHeroPreviewCoroutine()
-      {
-         yield return new WaitForSeconds(_displayDelay);
-         _heroPreviewVisual.LoadHeroPreviewVisuals.UpdateHeroPreviewAttributes();
-         _heroPreviewVisual.PreviewCanvas.gameObject.SetActive(true);
-         yield return null;
-
-      }
-
-      private IEnumerator NullAction()
-      {
-         yield return null;
-      }
 
    }
 }
