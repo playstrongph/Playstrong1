27cce698b94d54abce26fe0e07300820a07a3f75
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-05-31 15:16:22 +0800

    Skill Targetting WIP

--- Playstrong/Assets/Scripts/Logic/GetSkillTargets.cs
+++ Playstrong/Assets/Scripts/Logic/GetSkillTargets.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Interfaces;
 using UnityEngine;
+using Utilities;
 
 namespace Logic
 {
@@ -15,9 +17,102 @@ namespace Logic
         private List<IHero> _enemyNormalHeroes = new List<IHero>();
         private List<IHero> _enemyTauntHeroes = new List<IHero>();
         private List<IHero> _enemyStealthHeroes = new List<IHero>();
+
+        /// <summary>
+        /// This is set by the skillTarget Asset
+        /// Can either be EnemyTargets or AllyTargets
+        /// </summary>
+        private Action _getValidTargets;
+
+        private void Awake()
+        {
+            _targetSkill = GetComponent<ITargetSkill>();
+            _thisHero = _targetSkill.Skill.Hero;
+        }
+        
+        public List<IHero> GetValidTargets()
+        {
+            //Note: Sequence of method calls is important
+            _validTargets.Clear();
+
+            _getValidTargets();
+            
+            return _validTargets;
+        }
+
+
+        private void GetAllyTargets()
+        {
+            var allies = _thisHero.LivingHeroes.HeroesList;
+            foreach (var ally in allies)
+            {
+                var allyHero = ally.GetComponent<IHero>();
+                _validTargets.Add(allyHero);
+            }
+        }
+        
+        private void GetEnemyTargets()
+        {
+            SetnemyTargetLists();
+            SetEnemyStealthTargets();
+            SetEnemyTauntTargets();
+            SetEnemyNormalTargets();
+        }
+        
+        private void SetnemyTargetLists()
+        {
+            var enemies = _thisHero.LivingHeroes.Player.OtherPlayer.LivingHeroes.HeroesList;
+            foreach (var enemy in enemies)
+            {
+                var enemyHero = enemy.GetComponent<IHero>();
+                enemyHero.HeroLogic.TargetStatus.AddToTargetList(enemyHero,_enemyNormalHeroes, _enemyTauntHeroes, _enemyStealthHeroes);
+            }
+        }   
+        
+        private void SetEnemyStealthTargets()
+        {
+            foreach (var enemy in _enemyStealthHeroes)
+            {
+                //Do stealth effects here.
+            }
+        }
+        
+        private void SetEnemyTauntTargets()
+        {
+            foreach (var enemy in _enemyTauntHeroes)
+            {
+                _enemyNormalHeroes.Clear();
+                _validTargets.Add(enemy);
+            }
+        }
+        
+        private void SetEnemyNormalTargets()
+        {
+            foreach (var enemy in _enemyNormalHeroes)
+            {
+                _validTargets.Add(enemy);
+            }
+        }
+        
+        
+
+        public void NoAction()
+        {
+            Debug.Log("No Valid Target List was set");
+        }
+
+
+
+
+
+
+
+
+
+
         //New List Based on Allowed SkillTargets
-        
-        
+
+
 
 
 
