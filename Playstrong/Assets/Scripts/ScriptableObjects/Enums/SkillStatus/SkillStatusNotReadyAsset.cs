e4132081cb471b053b15278b73bf57922a1f057a
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-06-01 14:16:32 +0800

    Skill Status WIP

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/Enums/SkillStatus/SkillStatusNotReadyAsset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Enums.SkillStatus
+{
+    [CreateAssetMenu(fileName = "SkillStatusNotReady", menuName = "SO's/Scriptable Enums/Skill Status/Skill Status NotReady")]
+    public class SkillStatusNotReadyAsset : ScriptableObject
+    {
+        
+       
+        
+        
+
+    }
+}
---
+++ Playstrong/Assets/Scripts/ScriptableObjects/Enums/SkillStatus/SkillStatusReadyAsset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Enums.SkillStatus
+{
+    [CreateAssetMenu(fileName = "SkillStatusReady", menuName = "SO's/Scriptable Enums/Skill Status/Skill Status Ready")]
+    public class SkillStatusReadyAsset : ScriptableObject
+    {
+        
+       
+        
+        
+
+    }
+}
