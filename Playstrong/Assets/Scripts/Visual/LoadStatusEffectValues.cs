154fb80f467af6f07ab5d559c209bea92d338b91
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-06-20 14:52:06 +0800

    Update LoadStatusEffectValues.cs

--- Playstrong/Assets/Scripts/Visual/LoadStatusEffectValues.cs
+++ Playstrong/Assets/Scripts/Visual/LoadStatusEffectValues.cs
@@ -19,6 +19,7 @@ namespace Visual
          
             _heroStatusEffect.StatusEffectAsset = statusEffect;
             _heroStatusEffect.StatusEffectType = statusEffect.StatusEffectType;
+            _heroStatusEffect.StatusEffectCounterUpdate = statusEffect.CounterUpdate;
 
             _heroStatusEffect.Counters = counters;
             _heroStatusEffect.Icon.sprite = statusEffect.Icon;
