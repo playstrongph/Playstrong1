a52949c2081adfbc7b8af90bf8a03cd04bba52c6
playstrongph <playstrong.ph@gmail.com>
2021-06-21 08:02:13 +0800

    Status Effect Instances WIP

--- Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/Instance/SingleInstance.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/Instance/SingleInstance.cs
@@ -10,38 +10,65 @@ namespace ScriptableObjects.StatusEffects.Instance
     [CreateAssetMenu(fileName = "SingleInstance", menuName = "SO's/Status Effects/Instance/Single Instance")]
     public class SingleInstance : ScriptableObject, IStatusEffectInstance
     {
-
-        public void AddStatusEffect(IHero hero)
+        private IHeroStatusEffect _existingStatusEffect;
+        
+        public void AddStatusEffect(IHero hero,  IHeroStatusEffect addStatusEffect, IStatusEffectAsset statusEffectAsset, int statusEffectCounters)
         {
-            if(CheckStatusEffectExistence(hero))
-                UpdateStatusEffect();
+            if(CheckExistingStatusEffects(hero, addStatusEffect))
+                UpdateStatusEffect(addStatusEffect);
             else
-                CreateStatusEffect();
+                CreateStatusEffect(hero, statusEffectAsset, statusEffectCounters);
             
         }
         
         
-        public bool CheckStatusEffectExistence(IHero hero)
+        private bool CheckExistingStatusEffects(IHero hero, IHeroStatusEffect addStatusEffect)
         {
-            IHeroStatusEffect statusEffect;
-            foreach (var buff in hero.HeroStatusEffects.HeroBuffEffects.HeroBuffs )
+            var statusEffectExists = false;
+            foreach (var statusEffect in hero.HeroStatusEffects.HeroBuffEffects.HeroBuffs )
             {
-              
-                    
+                if (addStatusEffect.StatusEffectType == statusEffect.StatusEffectType)
+                {
+                    statusEffectExists = true;
+                    _existingStatusEffect = statusEffect;
+                }
             }
             
-            var statusEffectExists = false;
+            foreach (var statusEffect in hero.HeroStatusEffects.HeroDebuffEffects.HeroDebuffs )
+            {
+                if (addStatusEffect.StatusEffectType == statusEffect.StatusEffectType)
+                {
+                    statusEffectExists = true;
+                    _existingStatusEffect = statusEffect;
+                }
+            }
+
             return statusEffectExists;
         }
 
-        public void CreateStatusEffect()
+        private void CreateStatusEffect(IHero hero, IStatusEffectAsset statusEffectAsset, int statusEffectCounters)
         {
+            var buffEffectPrefab = hero.HeroStatusEffects.HeroStatusEffectPrefab;
+            var statusEffectPanel = hero.HeroStatusEffects.StatusEffectsPanel.Transform;
             
+            var buffEffectObject = Instantiate(buffEffectPrefab, statusEffectPanel);
+            buffEffectObject.transform.SetParent(statusEffectPanel);
+            var heroStatusEffect = buffEffectObject.GetComponent<IHeroStatusEffect>();
+
+            heroStatusEffect.LoadStatusEffectValues.LoadValues(statusEffectAsset, statusEffectCounters);
+            heroStatusEffect.StatusEffectAsset.ApplyStatusEffect();
+
+            //Add to respective StatusEffects List in HeroStatusEffects
+            heroStatusEffect.StatusEffectType.AddToStatusEffectsList(hero.HeroStatusEffects, heroStatusEffect);
         }
 
-        public void UpdateStatusEffect()
+        private void UpdateStatusEffect(IHeroStatusEffect addStatusEffect)
         {
-            
+            var addStatusEffectCounters = addStatusEffect.Counters;
+            var existingStatusEffectCounters = _existingStatusEffect.Counters;
+            var newCounters = Mathf.Max(addStatusEffectCounters, existingStatusEffectCounters);
+
+            _existingStatusEffect.Counters = newCounters;
         }
         
         
