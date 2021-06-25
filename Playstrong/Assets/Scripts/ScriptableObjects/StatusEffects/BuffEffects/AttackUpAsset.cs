420e00198c0f71eff8a7fc940e6750dd2a050167
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-06-25 15:56:45 +0800

    Update AttackUpAsset.cs

--- Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/AttackUpAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/AttackUpAsset.cs
@@ -6,19 +6,46 @@ namespace ScriptableObjects.StatusEffects.BuffEffects
     [CreateAssetMenu(fileName = "AttackUp", menuName = "SO's/Status Effects/Buffs/AttackUp")]
     public class AttackUpAsset : StatusEffectAsset
     {
+        private int _attackIncrease;
+        private float _multiplier = 0.5f;
+        
+        
+        private ICoroutineTree _logicTree;
+        private ICoroutineTree _visualTree;
+        
+        
         public override void ApplyStatusEffect(IHero hero)
         {
+            InitializeValues(hero);
+
+            //TODO: HeroLogic: Set Attack Logic
+            hero.HeroLogic.HeroAttributes.Attack += _attackIncrease;
+            hero.HeroVisual.AttackVisual.SetAttackText(hero.HeroLogic.HeroAttributes.Attack.ToString());
+                        
+            
             Debug.Log("Attack Up Buff");       
         }
         
         public override void UnapplyStatusEffect(IHero hero)
         {
+            hero.HeroLogic.HeroAttributes.Attack -= _attackIncrease;
+            hero.HeroVisual.AttackVisual.SetAttackText(hero.HeroLogic.HeroAttributes.Attack.ToString());
+            
             Debug.Log("Unapply Attack Up Buff");       
         }
-        
-        
 
-       
+        private void InitializeValues(IHero hero)
+        {
+            _logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            _visualTree = hero.CoroutineTreesAsset.MainVisualTree;
+
+            var baseAttack = hero.HeroLogic.HeroAttributes.BaseAttack;
+            _attackIncrease = Mathf.FloorToInt(_multiplier * baseAttack);
+        }
+
+
+
+
 
     }
 }
