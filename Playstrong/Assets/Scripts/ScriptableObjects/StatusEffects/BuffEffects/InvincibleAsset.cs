88a12642c49016d9dedc090cf3fe56ab4ffce048
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-06-28 14:44:28 +0800

    Buff Invincible Completed

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/InvincibleAsset.cs
@@ -0,0 +1,43 @@
+﻿using Interfaces;
+using ScriptableObjects.Modifiers;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.StatusEffects.BuffEffects
+{
+    [CreateAssetMenu(fileName = "Invincible", menuName = "SO's/Status Effects/Buffs/Invincible")]
+    public class InvincibleAsset : StatusEffectAsset
+    {
+        [Header("Invincible Modifier")]
+        [SerializeField] [RequireInterface(typeof(IModifier))]
+        private ScriptableObject damageModifier;
+        private IModifier DamageModifier => damageModifier as IModifier;
+
+
+        private ICoroutineTree _logicTree;
+        private ICoroutineTree _visualTree;
+        public override void ApplyStatusEffect(IHero hero)
+        {
+            InitializeValues(hero);
+            
+            hero.HeroLogic.TakeDamage.AddToDamageModifiersList(DamageModifier);
+        }
+        
+        public override void UnapplyStatusEffect(IHero hero)
+        {
+            hero.HeroLogic.TakeDamage.RemoveFromDamageModifiersList(DamageModifier);
+            
+        }
+
+        private void InitializeValues(IHero hero)
+        {
+            _logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            _visualTree = hero.CoroutineTreesAsset.MainVisualTree;
+        }
+
+
+
+
+
+    }
+}
