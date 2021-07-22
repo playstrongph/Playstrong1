c2684b1670e305d8909840d09a23afe400c0ff95
playstrongph <playstrong.ph@gmail.com>
2021-07-22 08:36:03 +0800

    CD Passive WIP

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/Enums/SkillType/CDPassiveSkillAsset.cs
@@ -0,0 +1,33 @@
+﻿using Interfaces;
+using Logic;
+using References;
+using TMPro;
+using UnityEngine;
+using Visual;
+
+namespace ScriptableObjects.Enums.SkillType
+{
+    [CreateAssetMenu(fileName = "CDPassiveSkill", menuName = "SO's/Skill Type/CDPassiveSkill")]
+    public class CdPassiveSkillAsset : SkillType
+    {
+        public override void SkillCooldownDisplay(TextMeshProUGUI cooldown)
+        {
+            cooldown.enabled = true;
+        }
+
+        public override void ReduceSkillCd(ISkill skill, int counter)
+        {
+            var logicTree = skill.CoroutineTreesAsset.MainLogicTree;
+            
+            logicTree.AddCurrent(skill.SkillLogic.ChangeSkillCooldown.ReduceCooldown(counter));
+        }
+        
+        public override void ResetSkillCd(ISkill skill)
+        {
+            var logicTree = skill.CoroutineTreesAsset.MainLogicTree;
+
+            logicTree.AddCurrent(skill.SkillLogic.ChangeSkillCooldown.ResetCooldown());
+        }
+
+    }
+}
