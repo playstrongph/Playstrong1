c842d3c12fee3d82b37b7b6f00d087ebc0401559
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-10 16:00:20 +0800

    AddBuff  Chance and Resistance

--- Playstrong/Assets/Scripts/ScriptableObjects/Actions/AddBuffSkillActionAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/Actions/AddBuffSkillActionAsset.cs
@@ -36,16 +36,23 @@ namespace ScriptableObjects.SkillActions
            yield return null;
 
         }
-        
-       
 
         private IEnumerator AddBuffCoroutine(IStatusEffectAsset statusEffectAsset, int statusEffectCounters)
         {
-           
-            BuffAsset.StatusEffectInstance.AddStatusEffect(TargetHero, statusEffectAsset, statusEffectCounters);
+            
+            //Check BuffResistances Here
+            var buffResistance = TargetHero.HeroLogic.OtherAttributes.BuffResistance;
+            var buffChance = TargetHero.HeroLogic.OtherAttributes.BuffChance;
+            var buffSuccess = buffChance - buffResistance;
+            var randomChance = Random.Range(0f, 100f);
+            
+            buffSuccess = Mathf.Clamp(buffSuccess, 0f, 100f);
+            
+            if(randomChance<= buffSuccess)
+                BuffAsset.StatusEffectInstance.AddStatusEffect(TargetHero, statusEffectAsset, statusEffectCounters);
        
             
-             LogicTree.EndSequence();
+            LogicTree.EndSequence();
             yield return null;
         }
 
