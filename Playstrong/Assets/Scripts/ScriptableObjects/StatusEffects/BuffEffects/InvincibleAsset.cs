4dc55e773508d9ffb7c16e352d5e7c366c11cd2a
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-11 12:01:27 +0800

    Update InvincibleAsset.cs

--- Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/InvincibleAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/InvincibleAsset.cs
@@ -8,21 +8,18 @@ namespace ScriptableObjects.StatusEffects.BuffEffects
     [CreateAssetMenu(fileName = "Invincible", menuName = "SO's/Status Effects/Buffs/Invincible")]
     public class InvincibleAsset : StatusEffectAsset
     {
-        [Header("Invincible Modifier")]
-        [SerializeField] [RequireInterface(typeof(IModifier))]
-        private ScriptableObject damageModifier;
-        private IModifier DamageModifier => damageModifier as IModifier;
 
+        [SerializeField] private int reductionValue = 100;
         public override void ApplyStatusEffect(IHero hero)
         {
-            InitializeValues(hero);
-            
-            //hero.HeroLogic.TakeDamage.AddToDamageModifiersList(DamageModifier);
+            var otherAttributes = hero.HeroLogic.OtherAttributes;
+            otherAttributes.DamageReduction += reductionValue;
         }
         
         public override void UnapplyStatusEffect(IHero hero)
         {
-            //hero.HeroLogic.TakeDamage.RemoveFromDamageModifiersList(DamageModifier);
+            var otherAttributes = hero.HeroLogic.OtherAttributes;
+            otherAttributes.DamageReduction -= reductionValue;
         }
 
        
