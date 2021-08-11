09148d6a8440281f9ac1725da73af84dbc2b8662
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-11 11:50:23 +0800

    Recovery Update

--- Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/RecoveryAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/RecoveryAsset.cs
@@ -12,7 +12,11 @@ namespace ScriptableObjects.StatusEffects.BuffEffects
     [CreateAssetMenu(fileName = "Recovery", menuName = "SO's/Status Effects/Buffs/Recovery")]
     public class RecoveryAsset : StatusEffectAsset
     {
-        [SerializeField] private float percentMultiplier = 15f;
+      
+
+        [SerializeField] private Object _healActionAsset;
+        
+        private IHeroAction HealActionAsset => _healActionAsset as IHeroAction;
 
         public override void ApplyStatusEffect(IHero hero)
         {
@@ -27,44 +31,22 @@ namespace ScriptableObjects.StatusEffects.BuffEffects
         private void RecoveryEffect(IHero hero)
         {
             var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
-            
-            var targetHealChance = hero.HeroLogic.OtherAttributes.HealChance;
-            var targetHealResistance = hero.HeroLogic.OtherAttributes.HealResistance;
-            var randomValue = Random.Range(1f, 100f);
-            var netHealChance = targetHealChance - targetHealResistance;
-            
-            netHealChance = Mathf.Clamp(netHealChance,0, 100);
-            
-            
-            if(randomValue <= netHealChance)
-                logicTree.AddCurrent(RecoveryEffectCoroutine(hero));
+
+            logicTree.AddCurrent(RecoveryEffectCoroutine(hero));
         }
 
 
         private IEnumerator RecoveryEffectCoroutine(IHero hero)
         {
             var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
-            var visualTree = hero.CoroutineTreesAsset.MainVisualTree;
-            
-            var healMultiplier = percentMultiplier / 100f;
-            var healValue = Mathf.FloorToInt(healMultiplier * hero.HeroLogic.HeroAttributes.BaseHealth);
-            var newHealth = hero.HeroLogic.HeroAttributes.Health + healValue;
-            
-            visualTree.AddCurrent(HealVisual(hero,healValue));
-            hero.HeroLogic.SetHeroHealth.SetHealth(newHealth);
+
+            logicTree.AddCurrent(HealActionAsset.ActionTarget(hero,hero));     
 
             logicTree.EndSequence();
             yield return null;
         }
         
-        private IEnumerator HealVisual(IHero hero, int value)
-        {
-            var visualTree = hero.CoroutineTreesAsset.MainVisualTree;
-            hero.DamageEffect.ShowDamage(value);
-
-            visualTree.EndSequence();
-            yield return null;
-        }
+       
 
 
 
