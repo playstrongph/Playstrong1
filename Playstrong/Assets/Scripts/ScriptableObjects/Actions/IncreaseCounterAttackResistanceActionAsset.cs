6515db4814b150372df1325ef9f52bd3163d20a6
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-13 14:54:41 +0800

    CounterAttack WIP

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/Actions/IncreaseCounterAttackResistanceActionAsset.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Interfaces;
+using ScriptableObjects.Actions.BaseClassScripts;
+using ScriptableObjects.Modifiers;
+using ScriptableObjects.Others;
+using ScriptableObjects.StatusEffects;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.Actions
+{
+    [CreateAssetMenu(fileName = "IncreaseCounterAttackResistanceActionAsset", menuName = "SO's/SkillActions/IncreaseCounterAttackResistanceActionAsset")]
+    
+    public class IncreaseCounterAttackResistanceActionAsset : SkillActionAsset
+    {
+        [SerializeField] private int counterAttackResistanceIncrease;
+
+       
+        public override IEnumerator StartAction(IHero targetHero, float value)
+        {
+            counterAttackResistanceIncrease = (int)value;
+            
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            targetHero.HeroLogic.OtherAttributes.CounterAttackResistance += counterAttackResistanceIncrease;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
