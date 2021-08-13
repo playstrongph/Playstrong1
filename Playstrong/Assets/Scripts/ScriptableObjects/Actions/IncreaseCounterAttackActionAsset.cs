832bcdc3e9bff5a24cf35de40a9b0808ba5bafd1
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-13 14:10:30 +0800

    CounterAttack Buff WIP

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/Actions/IncreaseCounterAttackActionAsset.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Interfaces;
+using ScriptableObjects.Actions.BaseClassScripts;
+using ScriptableObjects.Modifiers;
+using ScriptableObjects.Others;
+using ScriptableObjects.StatusEffects;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.Actions
+{
+    [CreateAssetMenu(fileName = "IncreaseCounterAttackActionAsset", menuName = "SO's/SkillActions/IncreaseCounterAttackActionAsset")]
+    
+    public class IncreaseCounterAttackActionAsset : SkillActionAsset
+    {
+        [SerializeField] private int counterAttackChanceIncrease;
+
+       
+        public override IEnumerator StartAction(IHero targetHero, float value)
+        {
+            counterAttackChanceIncrease = (int)value;
+            
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            targetHero.HeroLogic.OtherAttributes.CounterAttackChance += counterAttackChanceIncrease;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
