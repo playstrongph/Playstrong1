6b5549de3d8a9572df47de4a389e380b35e9ddf1
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-16 18:10:29 +0800

    Lucky

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/Actions/IncreaseSkillChanceBonusActionAsset.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Interfaces;
+using ScriptableObjects.Actions.BaseClassScripts;
+using ScriptableObjects.Modifiers;
+using ScriptableObjects.Others;
+using ScriptableObjects.StatusEffects;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.Actions
+{
+    [CreateAssetMenu(fileName = "IncreaseSkillChanceBonus", menuName = "SO's/SkillActions/IncreaseSkillChanceBonus")]
+    
+    public class IncreaseSkillChanceBonusActionAsset : SkillActionAsset
+    {
+        [SerializeField] private int skillChanceBonusIncrease;
+
+       
+        public override IEnumerator StartAction(IHero targetHero, float value)
+        {
+            skillChanceBonusIncrease = (int)value;
+            
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            targetHero.HeroLogic.OtherAttributes.SkillChanceBonus += skillChanceBonusIncrease;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/LuckyAsset.cs
@@ -0,0 +1,32 @@
+﻿using Interfaces;
+using ScriptableObjects.Modifiers;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.StatusEffects.BuffEffects
+{
+    [CreateAssetMenu(fileName = "Lucky", menuName = "SO's/Status Effects/Buffs/Lucky")]
+    public class LuckyAsset : StatusEffectAsset
+    {
+
+        [SerializeField] private float luckyValue = 100f;
+        public override void ApplyStatusEffect(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, luckyValue));
+        }
+        
+        public override void UnapplyStatusEffect(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, -luckyValue));
+        }
+
+       
+
+
+
+
+
+    }
+}
