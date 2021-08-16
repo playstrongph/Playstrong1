281a52bdfab8b837da627438ac738b784a64342f
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-16 15:54:27 +0800

    Immunity

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/Actions/IncreaseDebuffResistanceActionAsset.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Interfaces;
+using ScriptableObjects.Actions.BaseClassScripts;
+using ScriptableObjects.Modifiers;
+using ScriptableObjects.Others;
+using ScriptableObjects.StatusEffects;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.Actions
+{
+    [CreateAssetMenu(fileName = "IncreaseDebuffResistanceActionAsset", menuName = "SO's/SkillActions/IncreaseDebuffResistanceActionAsset")]
+    
+    public class IncreaseDebuffResistanceActionAsset : SkillActionAsset
+    {
+        [SerializeField] private int debuffResistanceIncrease;
+
+       
+        public override IEnumerator StartAction(IHero targetHero, float value)
+        {
+            debuffResistanceIncrease = (int)value;
+            
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            targetHero.HeroLogic.OtherAttributes.DebuffResistance += debuffResistanceIncrease;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/ImmunityAsset.cs
@@ -0,0 +1,32 @@
+﻿using Interfaces;
+using ScriptableObjects.Modifiers;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.StatusEffects.BuffEffects
+{
+    [CreateAssetMenu(fileName = "Immunity", menuName = "SO's/Status Effects/Buffs/Immunity")]
+    public class ImmunityAsset : StatusEffectAsset
+    {
+
+        [SerializeField] private float debuffResistanceValue = 200f;
+        public override void ApplyStatusEffect(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, debuffResistanceValue));
+        }
+        
+        public override void UnapplyStatusEffect(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, -debuffResistanceValue));
+        }
+
+       
+
+
+
+
+
+    }
+}
