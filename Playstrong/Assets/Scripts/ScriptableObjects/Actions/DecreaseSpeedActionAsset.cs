5af0b27a278abdc9c2b2e3345e6324c9cacb984a
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-23 14:06:46 +0800

    Slow Debuff

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/Actions/DecreaseSpeedActionAsset.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using Interfaces;
+using ScriptableObjects.Actions.BaseClassScripts;
+using ScriptableObjects.Modifiers;
+using ScriptableObjects.Others;
+using ScriptableObjects.StatusEffects;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.Actions
+{
+    [CreateAssetMenu(fileName = "DecreaseSpeedAction", menuName = "SO's/SkillActions/DecreaseSpeedAction")]
+    
+    public class DecreaseSpeedActionAsset : SkillActionAsset
+    {
+        [SerializeField] private int speedDecrease;
+
+       
+        public override IEnumerator ActionTarget(IHero targetHero, float value)
+        {
+            speedDecrease = (int)value;
+            
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            var newSpeedValue = targetHero.HeroLogic.HeroAttributes.Speed - speedDecrease;
+            targetHero.HeroLogic.SetHeroSpeed.SetSpeed(newSpeedValue);
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
