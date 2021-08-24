64b69b6d57c395672262177c9029eecbf1ae7062
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-24 14:16:46 +0800

    Poison

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/DebuffEffect/PoisonAsset.cs
@@ -0,0 +1,45 @@
+﻿using Interfaces;
+using Logic;
+using ScriptableObjects.Modifiers;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.StatusEffects.BuffEffects
+{
+    [CreateAssetMenu(fileName = "Poison", menuName = "SO's/Status Effects/Debuffs/Poison")]
+    public class PoisonAsset : StatusEffectAsset
+    {
+        [SerializeField] private float poisonDamage = 0;
+        [SerializeField] private float percentFactor = 5f;
+
+        public override void ApplyStatusEffect(IHero hero)
+        {
+            hero.HeroLogic.HeroEvents.EHeroStartTurn += PoisonHeroEffect;
+        }
+        
+        public override void UnapplyStatusEffect(IHero hero)
+        {
+            hero.HeroLogic.HeroEvents.EHeroStartTurn -= PoisonHeroEffect;
+            
+        }
+
+        private void PoisonHeroEffect(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            
+            poisonDamage = percentFactor * hero.HeroLogic.HeroAttributes.BaseHealth / 100;
+            poisonDamage = Mathf.CeilToInt(poisonDamage);
+            
+            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, poisonDamage));
+            
+        }
+
+
+
+
+
+
+
+
+    }
+}
