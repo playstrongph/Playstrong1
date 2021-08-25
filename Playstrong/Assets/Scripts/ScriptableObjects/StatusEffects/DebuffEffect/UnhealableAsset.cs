ebfec162afda1b3ff833932451bdedfac1b2f5b5
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-25 12:15:53 +0800

    Unhealable

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/DebuffEffect/UnhealableAsset.cs
@@ -0,0 +1,26 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace ScriptableObjects.StatusEffects.DebuffEffect
+{
+    [CreateAssetMenu(fileName = "Unhealable", menuName = "SO's/Status Effects/Debuffs/Unhealable")]
+    public class UnhealableAsset : StatusEffectAsset
+    {
+
+        [SerializeField] private int unhealableResistance = 1000;
+        
+        public override void ApplyStatusEffect(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, unhealableResistance));
+        }
+
+        public override void UnapplyStatusEffect(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, unhealableResistance));
+        }
+
+        
+    }
+}
