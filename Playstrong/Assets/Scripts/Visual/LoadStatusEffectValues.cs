f1d353a1dc5f6aa952c3d50c0e76065018879fd4
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-08-30 15:57:47 +0800

    Update LoadStatusEffectValues.cs

--- Playstrong/Assets/Scripts/Visual/LoadStatusEffectValues.cs
+++ Playstrong/Assets/Scripts/Visual/LoadStatusEffectValues.cs
@@ -17,7 +17,14 @@ namespace Visual
         public void LoadValues(IStatusEffectAsset statusEffect, int counters)
         {
          
-            _heroStatusEffect.StatusEffectAsset = statusEffect;
+            //TEST
+            var statusEffectObject = statusEffect as ScriptableObject;
+            var uniqueStatusEffectObject = Instantiate(statusEffectObject);
+            var uniqueStatusEffect = uniqueStatusEffectObject as IStatusEffectAsset;
+
+            //_heroStatusEffect.StatusEffectAsset = statusEffect;
+            _heroStatusEffect.StatusEffectAsset = CloneStatusEffectAsset(statusEffect);
+            
             _heroStatusEffect.StatusEffectType = statusEffect.StatusEffectType;
             _heroStatusEffect.StatusEffectCounterUpdate = statusEffect.UpdateTiming;
             _heroStatusEffect.StatusEffectInstance = statusEffect.StatusEffectInstance;
@@ -25,10 +32,17 @@ namespace Visual
             _heroStatusEffect.Counters = counters;
             _heroStatusEffect.Icon.sprite = statusEffect.Icon;
             _heroStatusEffect.CounterVisual.text = _heroStatusEffect.Counters.ToString();
-
-         
-
-
         }
+
+        private IStatusEffectAsset CloneStatusEffectAsset(IStatusEffectAsset statusEffect)
+        {
+            var statusEffectObject = statusEffect as ScriptableObject;
+            var statusEffectCloneObject = Instantiate(statusEffectObject);
+            var statusEffectClone = statusEffectCloneObject as IStatusEffectAsset;
+
+            return statusEffectClone;
+        }
+
+
     }
 }
