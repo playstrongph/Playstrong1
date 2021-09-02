a07711b28162b1db5534615919070d6bcd3bc409
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-09-02 15:44:35 +0800

    DebuffExtension WIP

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/SkillActionsScripts/IncreaseAllDebuffDurationsActionAsset.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Interfaces;
+using References;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using UnityEngine;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "IncreaseAllDebuffDurations", menuName = "SO's/SkillActions/IncreaseAllDebuffDurations")]
+    
+    public class IncreaseAllDebuffDurationsActionAsset : SkillActionAsset
+    {
+        [SerializeField] private int debuffDurationIncrease;
+        
+        public override IEnumerator ActionTarget(IHero targetHero, float value)
+        {
+            debuffDurationIncrease = (int)value;
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+            var allDebuffs = targetHero.HeroStatusEffects.HeroDebuffEffects.HeroDebuffs;
+            var coroutineTrees = targetHero.CoroutineTreesAsset;
+
+            foreach (var debuff in allDebuffs)
+            {
+                debuff.StatusEffectInstance.IncreaseCounters(debuff, targetHero,debuffDurationIncrease);
+            }
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
