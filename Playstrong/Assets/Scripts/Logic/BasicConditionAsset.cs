40e8240bb434d4ef9b2025ea83db7e576a8739b2
playstrongph <playstrong.ph@gmail.com>
2021-09-10 14:06:16 +0800

    BasicCondtionAsset

---
+++ Playstrong/Assets/Scripts/Logic/BasicConditionAsset.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Interfaces;
+using UnityEngine;
+
+namespace Logic
+{
+    public class BasicConditionAsset : ScriptableObject
+    {
+       
+        /// <summary>
+        /// Returns a value of 1 if basic condition is met, 0 otherwise;
+        /// Default value is zero
+        /// </summary>
+        public int GetValue(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            var value = CheckBasicCondition(thisHero,targetHero);
+            
+            return value;
+        }
+        
+        protected virtual int CheckBasicCondition(IHero thisHero, IHero targetHero)
+        {
+            var x = 0;
+            return x;
+        }
+
+        
+        
+        
+        
+    }
+}
