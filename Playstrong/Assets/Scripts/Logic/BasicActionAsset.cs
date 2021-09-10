6dd81c74dd4f0814ffc09347d80dad57c02931b2
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-09-10 12:04:15 +0800

    Update BasicActionAsset.cs

--- Playstrong/Assets/Scripts/Logic/BasicActionAsset.cs
+++ Playstrong/Assets/Scripts/Logic/BasicActionAsset.cs
@@ -25,5 +25,45 @@ namespace Logic
             yield return null;
         }
 
+        public IEnumerator StartAction(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            
+            //TODO: Implement in HeroLivingStatus - IBasicActionAsset
+            //targetHero.HeroLogic.HeroLivingStatus.ReceiveHeroAction(this, thisHero, targetHero);
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        public virtual IEnumerator TargetAction(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+        public IEnumerator StartAction(IHero hero, float value)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            
+            //TODO: Implement in HeroLivingStatus - IBasicActionAsset
+            //targetHero.HeroLogic.HeroLivingStatus.ReceiveHeroAction(this, thisHero, targetHero);
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        public virtual IEnumerator TargetAction(IHero hero, float value)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        
+
     }
 }
