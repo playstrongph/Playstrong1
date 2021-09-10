84d3f582a25d85d0e769930c7191375b2ed73260
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-09-10 12:00:06 +0800

    StandardAction WIP

---
+++ Playstrong/Assets/Scripts/Logic/BasicActionAsset.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Interfaces;
+using UnityEngine;
+
+namespace Logic
+{
+    public class BasicActionAsset : ScriptableObject
+    {
+        public IEnumerator StartAction(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            
+            //TODO: Implement in HeroLivingStatus - IBasicActionAsset
+            //targetHero.HeroLogic.HeroLivingStatus.ReceiveHeroAction(this, thisHero, targetHero);
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        public virtual IEnumerator TargetAction(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+    }
+}
