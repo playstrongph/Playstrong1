6f0a37f3c527386eae53de4f847112885841741b
playstrongph <playstrong.ph@gmail.com>
2021-09-11 16:41:30 +0800

    Update IStandardActionAsset.cs

--- Playstrong/Assets/Scripts/ScriptableObjects/StandardActions/IStandardActionAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/StandardActions/IStandardActionAsset.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using Interfaces;
+using References;
 
 namespace ScriptableObjects.StandardActions
 {
     public interface IStandardActionAsset
     {
-        
+        IEnumerator RegisterStandardAction(IHero hero);
+        IEnumerator UnregisterStandardAction(IHero hero);
+        IEnumerator RegisterStandardAction(ISkill skill);
+        IEnumerator UnregisterStandardAction(ISkill skill);
+        void StartAction(IHero targetHero);
+        void StartAction(IHero thisHero, IHero targetHero);
     }
 }
