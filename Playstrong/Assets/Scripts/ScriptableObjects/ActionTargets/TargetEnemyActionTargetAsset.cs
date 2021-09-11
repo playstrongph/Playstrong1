dd06fa386f709157176702e689b3ebff3a8c5da7
playstrongph <playstrong.ph@gmail.com>
2021-09-11 17:50:47 +0800

    Stadnard Actioin Sample

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/ActionTargets/TargetEnemyActionTargetAsset.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Interfaces;
+using Logic;
+using UnityEngine;
+
+namespace ScriptableObjects.ActionTargets
+{
+    
+    [CreateAssetMenu(fileName = "TargetEnemy", menuName = "SO's/ActionTargets/TargetEnemy")]
+    public class TargetEnemyActionTargetAsset : ActionTargetsAsset
+    {
+        public override List<IHero> GetHeroTargets(IHero thisHero, IHero targetHero)
+        {
+            var heroTargets = new List<IHero>();
+            heroTargets.Clear();
+            
+            heroTargets.Add(targetHero);
+            
+            return heroTargets;
+        }
+        
+        public override List<IHero> GetHeroTargets(IHero targetHero)
+        {
+            var heroTargets = new List<IHero>();
+            heroTargets.Clear();
+            
+            heroTargets.Add(targetHero);
+            
+            return heroTargets;
+        }
+            
+    }
+}
---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActions/AttackBasicActionAsset.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using ScriptableObjects.AnimationSOscripts;
+using ScriptableObjects.Enums.SkillStatus;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using UnityEngine;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "AttackBasicAction", menuName = "SO's/BasicActions/AttackBasicAction")]
+    
+    public class AttackBasicActionAsset : BasicActionAsset
+    {
+        [Header("Game Animation Asset")]
+        [SerializeField] private ScriptableObject attackAnimation;
+        private IGameAnimations AttackAnimation => attackAnimation as IGameAnimations;
+
+        private int _finalAttackValue;
+        private float _intervalDelay = 0.7f;
+
+        [Header("Attack Target Type")] [SerializeField]
+        private ScriptableObject attackTargetType;
+        private IAttackTargetTypeAsset AttackTargetType => attackTargetType as IAttackTargetTypeAsset;
+        
+
+        public override IEnumerator TargetAction(IHero thisHero, IHero targetHero)
+        {
+           
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+
+            logicTree.AddCurrent(AttackHero(thisHero,targetHero));
+
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+
+        private IEnumerator AttackHero(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            var dealDamage = targetHero.HeroLogic.DealDamage;
+            var attackPower = thisHero.HeroLogic.HeroAttributes.Attack;
+            var criticalFactor = 0;
+
+            
+            logicTree.AddCurrent(AttackHeroAnimation(thisHero,targetHero));
+
+            logicTree.AddCurrent(AttackTargetType.DealAttackDamage(dealDamage,thisHero, targetHero, attackPower, criticalFactor));
+
+            logicTree.AddCurrent(AttackInterval(thisHero,targetHero));
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+        private IEnumerator AttackHeroAnimation(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            var visualTree = thisHero.CoroutineTreesAsset.MainVisualTree;
+
+            visualTree.AddCurrent(AttackAnimation.StartAnimation(thisHero,targetHero));
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+
+        private IEnumerator AttackInterval(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            var visualTree = thisHero.CoroutineTreesAsset.MainVisualTree;
+            
+           //Inserts delay in seconds before calling visualTree.EndSequence()
+            visualTree.AddCurrentWait(_intervalDelay, visualTree);
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        
+
+
+       
+
+
+
+
+
+
+
+    }
+}
