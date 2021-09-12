a90a828ca2807bb6543b01666d4f12ef92ca1cc3
playstrongph <playstrong.ph@gmail.com>
2021-09-12 10:55:20 +0800

    Sweep WIP

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActions/DecreaseEnergyBasicActionAsset.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using UnityEngine;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "DecreaseEnergy", menuName = "SO's/BasicActions/DecreaseEnergy")]
+    
+    public class DecreaseEnergyBasicActionAsset : BasicActionAsset
+    {
+        [SerializeField] private int energyDecrease;
+        
+        public override IEnumerator TargetAction(IHero targetHero)
+        {
+           
+            
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+            var reduceEnergyChance = targetHero.HeroLogic.OtherAttributes.BoostEnergyChance;
+            var reduceEnergyResistance = targetHero.HeroLogic.OtherAttributes.BoostEnergyResistance;
+            var netBoostChance = reduceEnergyChance - reduceEnergyResistance;
+            var randomChance = Random.Range(0, 101);
+
+            if(randomChance<=netBoostChance)
+                DecreaseEnergy(targetHero, energyDecrease);
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator TargetAction(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+            var reduceEnergyChance = targetHero.HeroLogic.OtherAttributes.BoostEnergyChance;
+            var reduceEnergyResistance = targetHero.HeroLogic.OtherAttributes.BoostEnergyResistance;
+            var netBoostChance = reduceEnergyChance - reduceEnergyResistance;
+            var randomChance = Random.Range(0, 101);
+
+            if(randomChance<=netBoostChance)
+                DecreaseEnergy(targetHero, energyDecrease);
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+        private void DecreaseEnergy(IHero targetHero, float value)
+        {
+            var minusEnergy = (int)value;
+            targetHero.HeroLogic.SetHeroEnergy.DecreaseEnergy(minusEnergy);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
