739d7cf8cd8a8f2b324187ddd20b24df0c76b445
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-09-17 15:53:14 +0800

    BoilingBlood

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActions/AddBuffBasicActionAsset.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using ScriptableObjects.StatusEffects;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "AddBuffAction", menuName = "SO's/BasicActions/AddBuffAction")]
+    
+    public class AddBuffBasicActionAsset : BasicActionAsset 
+    {
+
+        [SerializeField]
+        [RequireInterface(typeof(IStatusEffectAsset))]
+        private ScriptableObject _buffEffectAsset;
+        private IStatusEffectAsset BuffAsset => _buffEffectAsset as IStatusEffectAsset;
+
+        [SerializeField] private int _buffCounters;
+        private int BuffCounters => _buffCounters;
+
+        /// <summary>
+        /// Logic of AddBuff is in the statuseffect asset Instance
+        /// Single, Multiple, Fixed
+        /// </summary>
+        public override IEnumerator TargetAction(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            
+            logicTree.AddCurrent(AddBuffCoroutine(BuffAsset, BuffCounters,thisHero,targetHero));
+           
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+
+        private IEnumerator AddBuffCoroutine(IStatusEffectAsset statusEffectAsset, int statusEffectCounters, IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            //Check BuffResistances Here
+            var buffResistance = targetHero.HeroLogic.OtherAttributes.BuffResistance;
+            var buffChance = targetHero.HeroLogic.OtherAttributes.BuffChance;
+            var buffSuccess = buffChance - buffResistance;
+            var randomChance = Random.Range(0f, 100f);
+            
+            buffSuccess = Mathf.Clamp(buffSuccess, 0f, 100f);
+            
+            if(randomChance<= buffSuccess)
+                BuffAsset.StatusEffectInstance.AddStatusEffect(targetHero, statusEffectAsset, statusEffectCounters,thisHero);
+       
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+
+      
+
+
+    }
+}
---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActions/AddDebuffBasicActionAsset.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using ScriptableObjects.StatusEffects;
+using UnityEngine;
+using Utilities;
+using Visual;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "AddDebuffAction", menuName = "SO's/BasicActions/AddDebuffAction")]
+    
+    public class AddDebuffBasicActionAsset : BasicActionAsset
+    {
+
+        [SerializeField]
+        [RequireInterface(typeof(IStatusEffectAsset))]
+        private ScriptableObject _debuffEffectAsset;
+        private IStatusEffectAsset DebuffAsset => _debuffEffectAsset as IStatusEffectAsset;
+
+        [SerializeField] private int _debuffCounters;
+        private int DebuffCounters => _debuffCounters;
+
+        /// <summary>
+        /// Logic of AddBuff is in the statuseffect asset Instance
+        /// Single, Multiple, Fixed
+        /// </summary>
+        public override IEnumerator TargetAction(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            logicTree.AddCurrent(AddDebuffCoroutine(DebuffAsset, DebuffCounters,thisHero,targetHero));
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+        private IEnumerator AddDebuffCoroutine(IStatusEffectAsset statusEffectAsset, int statusEffectCounters, IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            //Check BuffResistances Here
+            var debuffResistance = targetHero.HeroLogic.OtherAttributes.DebuffResistance;
+            var debuffChance = targetHero.HeroLogic.OtherAttributes.DebuffChance;
+            var debuffSuccess = debuffChance - debuffResistance;
+            var randomChance = Random.Range(0f, 100f);
+            
+            debuffSuccess = Mathf.Clamp(debuffSuccess, 0f, 100f);
+            
+            Debug.Log("Caster Hero: " +thisHero.HeroName);
+            
+            if(randomChance<= debuffSuccess)
+                DebuffAsset.StatusEffectInstance.AddStatusEffect(targetHero, statusEffectAsset, statusEffectCounters,thisHero);
+       
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+
+      
+
+
+    }
+}
