565977f071f5d5a9f8950de6f96dbdbb16902463
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-09-18 13:22:37 +0800

    Evasion

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActions/IncreaseCriticalResistanceBasicActionAsset.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using UnityEngine;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "IncreaseCriticalResistanceBasicAction", menuName = "SO's/BasicActions/IncreaseCriticalResistanceBasicAction")]
+    
+    public class IncreaseCriticalResistanceBasicActionAsset : BasicActionAsset
+    {
+        [SerializeField] private int criticalResistanceIncrease;
+
+       
+        public override IEnumerator TargetAction(IHero targetHero, float value)
+        {
+            criticalResistanceIncrease = (int)value;
+            
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            targetHero.HeroLogic.OtherAttributes.CriticalStrikeResistance += criticalResistanceIncrease;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/EvasionAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/EvasionAsset.cs
@@ -8,18 +8,21 @@ namespace ScriptableObjects.StatusEffects.BuffEffects
     [CreateAssetMenu(fileName = "Evasion", menuName = "SO's/Status Effects/Buffs/Evasion")]
     public class EvasionAsset : StatusEffectAsset
     {
-
         [SerializeField] private float resistanceValue = 200f;
         public override void ApplyStatusEffect(IHero hero)
         {
-            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
-            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, resistanceValue));
+            //var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            //logicTree.AddCurrent(SkillActionAsset.StartAction(hero, resistanceValue));
+            
+            StandardAction.StartAction(hero,resistanceValue);
         }
         
         public override void UnapplyStatusEffect(IHero hero)
         {
-            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
-            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, -resistanceValue));
+            //var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            //logicTree.AddCurrent(SkillActionAsset.StartAction(hero, -resistanceValue));
+            
+            StandardAction.StartAction(hero,-resistanceValue);
         }
 
        
