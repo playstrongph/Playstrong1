ebfc4418266d155a0a1d04e7a6b24269c3d6dc5f
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-09-25 14:45:22 +0800

    Update SingleTargetAttackAsset.cs

--- Playstrong/Assets/Scripts/ScriptableObjects/Enums/AttackTargetType/SingleTargetAttackAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/Enums/AttackTargetType/SingleTargetAttackAsset.cs
@@ -13,18 +13,34 @@ namespace ScriptableObjects.Enums.AttackTargetType
         public override IEnumerator DealAttackDamage(IDealDamage dealDamage, IHero thisHero, IHero targetHero, int attackPower, float criticalFactor)
         {
             var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
-
+            
+            logicTree.AddCurrent(PreSingleAttackEvents(thisHero,targetHero));
+            
             logicTree.AddCurrent(dealDamage.DealSingleAttackDamage(thisHero, targetHero, attackPower, criticalFactor));
+            
+            logicTree.AddCurrent(PostSingleAttackEvents(thisHero,targetHero));
             
             logicTree.EndSequence();
             yield return null;
         }
         
-        private IEnumerator BeforeHeroDealsSingleAttack(IHero thisHero, IHero targetHero)
+        private IEnumerator PreSingleAttackEvents(IHero thisHero, IHero targetHero)
         {
             var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
             
             thisHero.HeroLogic.HeroEvents.BeforeHeroDealsSingleAttack(thisHero);
+            targetHero.HeroLogic.HeroEvents.BeforeHeroTakesSingleAttack(thisHero);
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        private IEnumerator PostSingleAttackEvents(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            
+            thisHero.HeroLogic.HeroEvents.AfterHeroDealsSingleAttack(thisHero);
+            targetHero.HeroLogic.HeroEvents.AfterHeroTakesSingleAttack(thisHero);
             
             logicTree.EndSequence();
             yield return null;
