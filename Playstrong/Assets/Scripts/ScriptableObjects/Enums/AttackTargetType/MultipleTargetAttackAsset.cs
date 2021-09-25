f0f4a9ad30a4234612ab8f5fc6eda0c938e40b43
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-09-25 14:53:04 +0800

    Update MultipleTargetAttackAsset.cs

--- Playstrong/Assets/Scripts/ScriptableObjects/Enums/AttackTargetType/MultipleTargetAttackAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/Enums/AttackTargetType/MultipleTargetAttackAsset.cs
@@ -13,9 +13,35 @@ namespace ScriptableObjects.Enums.AttackTargetType
         public override IEnumerator DealAttackDamage(IDealDamage dealDamage, IHero thisHero, IHero targetHero, int attackPower, float criticalFactor)
         {
             var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            
+            logicTree.AddCurrent(PreMultiAttackEvents(thisHero,targetHero));
 
             logicTree.AddCurrent(dealDamage.DealMultipleAttackDamage(thisHero, targetHero, attackPower, criticalFactor));
+            
+            logicTree.AddCurrent(PostMultiAttackEvents(thisHero,targetHero));
 
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        private IEnumerator PreMultiAttackEvents(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            
+            thisHero.HeroLogic.HeroEvents.BeforeHeroDealsMultiAttack(thisHero);
+            targetHero.HeroLogic.HeroEvents.BeforeHeroTakesMultiAttack(thisHero);
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        private IEnumerator PostMultiAttackEvents(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            
+            thisHero.HeroLogic.HeroEvents.AfterHeroDealsMultiAttack(thisHero);
+            targetHero.HeroLogic.HeroEvents.AfterHeroTakesMultiAttack(thisHero);
+            
             logicTree.EndSequence();
             yield return null;
         }
