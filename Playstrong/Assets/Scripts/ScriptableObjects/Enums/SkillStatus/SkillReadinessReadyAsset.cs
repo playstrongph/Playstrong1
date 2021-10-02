2740954046fc43bc4b3fa25d4192e7329a0d1619
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-10-02 15:04:13 +0800

    Update SkillReadinessReadyAsset.cs

--- Playstrong/Assets/Scripts/ScriptableObjects/Enums/SkillStatus/SkillReadinessReadyAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/Enums/SkillStatus/SkillReadinessReadyAsset.cs
@@ -28,7 +28,7 @@ namespace ScriptableObjects.Enums.SkillStatus
             _logicTree.AddCurrent(skillType.SetSkillReady(skillLogic));
         }
         
-        
+        //Active Skill Set Ready
         public override IEnumerator SetActiveSkillReady(ISkillLogic skillLogic)
         {
             var logicTree = skillLogic.Skill.CoroutineTreesAsset.MainLogicTree;
@@ -43,7 +43,7 @@ namespace ScriptableObjects.Enums.SkillStatus
             yield return null;
         }
         
-       
+        //Passive Skill Ready
         public override IEnumerator SetCdPassiveSkillReady(ISkillLogic skillLogic)
         {
             var logicTree = skillLogic.Skill.CoroutineTreesAsset.MainLogicTree;
