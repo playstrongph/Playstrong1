8b41b1ee302b6fb91148611d35ea97545bc257a1
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-10-04 13:52:15 +0800

    Stun

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActions/IncreaseInabilityChanceBasicActionAsset.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using UnityEngine;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "IncreaseInabilityChance", menuName = "SO's/BasicActions/IncreaseInabilityChance")]
+    
+    public class IncreaseInabilityChanceBasicActionAsset : BasicActionAsset
+    {
+        [SerializeField] private int inabilityChance;
+
+        public override IEnumerator TargetAction(IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            targetHero.HeroLogic.OtherAttributes.HeroInabilityChance += inabilityChance;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator UndoTargetAction(IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            targetHero.HeroLogic.OtherAttributes.HeroInabilityChance -= inabilityChance;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/DebuffEffect/StunAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/DebuffEffect/StunAsset.cs
@@ -6,20 +6,15 @@ namespace ScriptableObjects.StatusEffects.DebuffEffect
     [CreateAssetMenu(fileName = "Stun", menuName = "SO's/Status Effects/Debuffs/Stun")]
     public class StunAsset : StatusEffectAsset
     {
-        [SerializeField]
-        private int _chanceIncrease = 150;
-
-        public override void ApplyStatusEffect(IHero hero) {
-       
-
-            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
-            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, _chanceIncrease));
+      
+        public override void ApplyStatusEffect(IHero hero) 
+        {
+            base.ApplyStatusEffect(hero);
         }
         
         public override void UnapplyStatusEffect(IHero hero)
         {
-            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
-            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, -_chanceIncrease));
+            base.UnapplyStatusEffect(hero);
         }
 
        
