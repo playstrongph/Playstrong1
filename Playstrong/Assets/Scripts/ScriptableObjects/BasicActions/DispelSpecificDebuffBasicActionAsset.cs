bb0db136aef89616d62f2d284d57b53cf4b8084c
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-10-04 14:54:46 +0800

    Sleep

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActions/DispelSpecificDebuffBasicActionAsset.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using ScriptableObjects.StatusEffects;
+using UnityEngine;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "DispelSpecificDebuff", menuName = "SO's/BasicActions/DispelSpecificDebuff")]
+    
+    public class DispelSpecificDebuffBasicActionAsset : BasicActionAsset
+    {
+        [SerializeField] private ScriptableObject _specificDebuff;
+        private IStatusEffectAsset SpecificDebuff => _specificDebuff as IStatusEffectAsset;
+
+        private IHeroStatusEffect _dispelDebuff;
+
+        public override IEnumerator TargetAction(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+
+            var debuffs = hero.HeroStatusEffects.HeroDebuffEffects.HeroDebuffs;
+            
+            foreach (var debuff in debuffs)
+            {
+                if (debuff.StatusEffectAsset.Name == SpecificDebuff.Name)
+                    _dispelDebuff = debuff;
+            }
+            
+            //Only dispels 1 debuff in case there are multiple instances (e.g. burn, poison)
+            if(_dispelDebuff != null)
+                _dispelDebuff.StatusEffectInstance.DispelStatusEffect(_dispelDebuff,hero);
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator TargetAction(IHero thisHero,IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+
+            var debuffs = thisHero.HeroStatusEffects.HeroDebuffEffects.HeroDebuffs;
+            
+            foreach (var debuff in debuffs)
+            {
+                if (debuff.StatusEffectAsset.Name == SpecificDebuff.Name)
+                    _dispelDebuff = debuff;
+            }
+            
+            //Only dispels 1 debuff in case there are multiple instances (e.g. burn, poison)
+            if(_dispelDebuff != null)
+                _dispelDebuff.StatusEffectInstance.DispelStatusEffect(_dispelDebuff,thisHero);
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+        public override IEnumerator UndoTargetAction(IHero hero)
+        {
+            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
+            
+            //No Action
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator UndoTargetAction(IHero thisHero,IHero targetHero)
+        {
+            var logicTree = thisHero.CoroutineTreesAsset.MainLogicTree;
+            
+            //No Action
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/DebuffEffect/SleepAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/DebuffEffect/SleepAsset.cs
@@ -7,48 +7,14 @@ namespace ScriptableObjects.StatusEffects.DebuffEffect
     [CreateAssetMenu(fileName = "Sleep", menuName = "SO's/Status Effects/Debuffs/Sleep")]
     public class SleepAsset : StatusEffectAsset
     {
-        [SerializeField]
-        private int _chanceIncrease = 150;
-
-        [SerializeField] private int _resistanceDecrease = 150;
-        
-
-        [SerializeField] private ScriptableObject decreaseCriticalResistance;
-        private IHeroAction DecreaseCriticalResistance => decreaseCriticalResistance as IHeroAction;
-        
-
-        public override void ApplyStatusEffect(IHero hero) {
-       
-
-            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
-            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, _chanceIncrease));
-            logicTree.AddCurrent(DecreaseCriticalResistance.StartAction(hero, _resistanceDecrease));
-
-            hero.HeroLogic.HeroEvents.EPostAttack += DispelSleep;
-
-        }
-        
-        public override void UnapplyStatusEffect(IHero hero)
+        public override void ApplyStatusEffect(IHero hero) 
         {
-            var logicTree = hero.CoroutineTreesAsset.MainLogicTree;
-            logicTree.AddCurrent(SkillActionAsset.StartAction(hero, -_chanceIncrease));
-            logicTree.AddCurrent(DecreaseCriticalResistance.StartAction(hero, -_resistanceDecrease));
-            
-            hero.HeroLogic.HeroEvents.EPostAttack -= DispelSleep;
+            base.ApplyStatusEffect(hero);
         }
-
-        private void DispelSleep(IHero hero, IHero dummyHero)
+        
+        public override void UnapplyStatusEffect(IHero hero) 
         {
-            HeroStatusEffectReference.RemoveStatusEffect.RemoveEffect(hero);
+            base.UnapplyStatusEffect(hero);
         }
-
-
-
-
-
-
-
-
-
     }
 }
