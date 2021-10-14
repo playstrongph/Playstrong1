cc9c8c86d30682568b5577fc47b884cbdbd139ee
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-10-14 13:32:46 +0800

    Buffs Debuffs Update

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActionScripts/RemoveStatusEffectBasicActionAsset.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using ScriptableObjects.StatusEffects;
+using UnityEngine;
+
+namespace ScriptableObjects.BasicActions
+{
+    
+    /// <summary>
+    /// This is different from Dispel - removal can't be prevented
+    /// </summary>
+    [CreateAssetMenu(fileName = "RemoveStatusEffect", menuName = "SO's/BasicActions/RemoveStatusEffect")]
+    public class RemoveStatusEffectBasicActionAsset : BasicActionAsset
+    {
+        [SerializeField] private ScriptableObject statusEffectAsset;
+        private IStatusEffectAsset StatusEffectAsset => statusEffectAsset as IStatusEffectAsset;
+        
+        
+        public override IEnumerator TargetAction(IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            RemoveHeroStatusEffect(targetHero);
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator UndoTargetAction(IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+            
+            //There is no UndoAction
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+        private void RemoveHeroStatusEffect(IHero targetHero)
+        {
+            var heroBuffEffects = targetHero.HeroStatusEffects.HeroBuffEffects.HeroBuffs;
+            var heroDebuffEffects = targetHero.HeroStatusEffects.HeroDebuffEffects.HeroDebuffs;
+
+            foreach (var buff in heroBuffEffects)
+            {
+                if (buff.Name == StatusEffectAsset.Name)
+                {
+                    buff.RemoveStatusEffect.RemoveEffect(targetHero);
+                }
+            }
+            
+            foreach (var debuff in heroDebuffEffects)
+            {
+                if (debuff.Name == StatusEffectAsset.Name)
+                {
+                    debuff.RemoveStatusEffect.RemoveEffect(targetHero);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffects/SmashAsset.cs
@@ -0,0 +1,28 @@
+﻿using Interfaces;
+using ScriptableObjects.Modifiers;
+using UnityEngine;
+using Utilities;
+
+namespace ScriptableObjects.StatusEffects.BuffEffects
+{
+    [CreateAssetMenu(fileName = "Smash", menuName = "SO's/Status Effects/Buffs/Smash")]
+    public class SmashAsset : StatusEffectAsset
+    {
+        public override void ApplyStatusEffect(IHero hero)
+        {
+            base.ApplyStatusEffect(hero);
+        }
+        
+        public override void UnapplyStatusEffect(IHero hero)
+        {
+            base.UnapplyStatusEffect(hero);
+        }
+
+       
+
+
+
+
+
+    }
+}
