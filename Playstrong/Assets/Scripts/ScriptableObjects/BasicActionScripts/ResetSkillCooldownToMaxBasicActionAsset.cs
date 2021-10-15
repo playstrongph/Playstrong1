12dd36e9bdc6aa12aa7be2278c16ed0f85efb454
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-10-15 14:13:08 +0800

    Update ResetSkillCooldownToMaxBasicActionAsset.cs

--- Playstrong/Assets/Scripts/ScriptableObjects/BasicActionScripts/ResetSkillCooldownToMaxBasicActionAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActionScripts/ResetSkillCooldownToMaxBasicActionAsset.cs
@@ -1,47 +1,35 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using Interfaces;
 using Logic;
 using References;
-using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
 using UnityEngine;
 
-namespace ScriptableObjects.SkillActionsScripts
+namespace ScriptableObjects.BasicActionScripts
 {
     [CreateAssetMenu(fileName = "ResetSkillCooldownToMax", menuName = "SO's/BasicActions/ResetSkillCooldownToMax")]
     
     public class ResetSkillCooldownToMaxBasicActionAsset : BasicActionAsset
     {
-        [SerializeField] private ScriptableObject _heroSkillAssetReference;
-        private IHeroSkillAsset HeroSkillAssetReference => _heroSkillAssetReference as IHeroSkillAsset;
+        [SerializeField] private ScriptableObject heroSkillAssetReference;
+        private IHeroSkillAsset HeroSkillAssetReference => heroSkillAssetReference as IHeroSkillAsset;
 
-        private ISkill skillToReset;
+        private ISkill _skillToReset;
 
         public override IEnumerator TargetAction(IHero targetHero)
         {
            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
            var heroSkillObjects = targetHero.HeroSkills.Skills.GetComponent<ISkillsPanel>().SkillList;
            var baseCooldown = HeroSkillAssetReference.BaseCooldown;
-           
-           Debug.Log("Skill To Reset Hero Reference: " +targetHero.HeroName);
 
            //Find Skill To Reset to Max Cooldown
            foreach (var heroSkillObject in heroSkillObjects)
            {
                var skill = heroSkillObject.GetComponent<ISkill>();
-               Debug.Log("Skill Name: " +skill.SkillName +" Skill Reference Name: " +HeroSkillAssetReference.SkillName);
-
                if (skill.SkillName == HeroSkillAssetReference.SkillName)
-                   skillToReset = skill;
+                   _skillToReset = skill;
            }
-
-           if (skillToReset != null)
-           {
-               Debug.Log("Skill To Reset " +skillToReset.SkillName);
-               logicTree.AddCurrent(skillToReset.SkillLogic.ChangeSkillCooldown.SetSkillCdToValue(baseCooldown));
-           }
-
            
+           logicTree.AddCurrent(_skillToReset.SkillLogic.ChangeSkillCooldown.SetSkillCdToValue(baseCooldown));
 
            logicTree.EndSequence();
            yield return null;
