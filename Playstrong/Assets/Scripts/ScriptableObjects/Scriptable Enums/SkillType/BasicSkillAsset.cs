e8e21544f6f0e5777d7f9726d3413ae5c036fd1a
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-10-21 14:26:44 +0800

    Arby Complete!

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/Scriptable Enums/SkillType/BasicSkillAsset.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using References;
+using TMPro;
+using UnityEngine;
+using Visual;
+
+namespace ScriptableObjects.Enums.SkillType
+{
+    [CreateAssetMenu(fileName = "BasicSkill", menuName = "SO's/Skill Type/BasicSkill")]
+    public class BasicSkillAsset : SkillType
+    {
+       public override void SkillCooldownDisplay(TextMeshProUGUI cooldown)
+        {
+            //cooldown.enabled = true;
+        }
+
+        public override void ReduceSkillCd(ISkill skill, int counter)
+        {
+            var logicTree = skill.CoroutineTreesAsset.MainLogicTree;
+            logicTree.AddCurrent(skill.SkillLogic.ChangeSkillCooldown.ReduceCooldown(counter));
+        }
+        
+        public override void ResetSkillCd(ISkill skill)
+        {
+            var logicTree = skill.CoroutineTreesAsset.MainLogicTree;
+            logicTree.AddCurrent(skill.SkillLogic.ChangeSkillCooldown.ResetCooldownToMax());
+        }
+        
+        public override IEnumerator SetSkillReady(ISkillLogic skillLogic)
+        {
+            var logicTree = skillLogic.Skill.CoroutineTreesAsset.MainLogicTree;
+            var visualTree = skillLogic.Skill.CoroutineTreesAsset.MainVisualTree;
+
+            logicTree.AddCurrent(EnableDragSkillTarget(skillLogic));
+            logicTree.AddCurrent(EnableTargetVisual(skillLogic));
+            visualTree.AddCurrent(VisualEnableSkillGlow(skillLogic));
+            visualTree.AddCurrent(HideCooldownText(skillLogic));
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+        public override IEnumerator SetSkillNotReady(ISkillLogic skillLogic)
+        {
+            var logicTree = skillLogic.Skill.CoroutineTreesAsset.MainLogicTree;
+            var visualTree = skillLogic.Skill.CoroutineTreesAsset.MainVisualTree;
+            
+            logicTree.AddCurrent(DisableDragSkillTarget(skillLogic));
+            logicTree.AddCurrent(DisableTargetVisual(skillLogic));
+            visualTree.AddCurrent(VisualDisableSkillGlow(skillLogic));
+            visualTree.AddCurrent(ShowCooldownText(skillLogic));
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+        public override IEnumerator SetSkillCdValue(ISkillLogic skillLogic, int counter)
+        {
+            var logicTree = skillLogic.Skill.CoroutineTreesAsset.MainLogicTree;
+            
+            logicTree.AddCurrent(skillLogic.ChangeSkillCooldown.SetSkillCdToValue(counter));
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator DisableActiveSkill(ISkill skill)
+        {
+            var logicTree = skill.CoroutineTreesAsset.MainLogicTree;
+            
+            //skill.SkillLogic.UpdateSkillEnabledStatus.SkillDisabled.DisableSkill(skill);
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator EnableActiveSkill(ISkill skill)
+        {
+            var logicTree = skill.CoroutineTreesAsset.MainLogicTree;
+
+            //skill.SkillLogic.UpdateSkillEnabledStatus.SkillEnabled.EnableSkill(skill);
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+    }
+}
