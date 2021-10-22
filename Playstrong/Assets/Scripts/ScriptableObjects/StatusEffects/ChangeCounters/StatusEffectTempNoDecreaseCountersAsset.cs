3aa8fec1ba54556cff7cd23f569a8d7afa0ff83c
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-10-22 15:52:37 +0800

    StatusEffect Counters Update Fix

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/ChangeCounters/StatusEffectTempNoDecreaseCountersAsset.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Interfaces;
+using Logic;
+using UnityEngine;
+using Visual;
+
+namespace ScriptableObjects.StatusEffects.Instance
+{
+    [CreateAssetMenu(fileName = "TempNoDecrease", menuName = "SO's/Status Effects/ChangeCounters/TempNoDecrease")]
+    public class StatusEffectTempNoDecreaseCountersAsset : StatusEffectChangeCountersAsset
+    {
+
+        public override void IncreaseStatusEffectCounters(IHeroStatusEffect heroStatusEffect,int counters)
+        {
+            heroStatusEffect.IncreaseStatusEffectCounters.IncreaseCounters(counters);
+        }
+        
+        public override void DecreaseStatusEffectCounters(IHeroStatusEffect heroStatusEffect,int counters)
+        {
+            //heroStatusEffect.DecreaseStatusEffectCounters.DecreaseCounters(counters);
+            
+        }
+
+        public override void SetStatusEffectCountersToValue(IHeroStatusEffect heroStatusEffect,int counters)
+        {
+            heroStatusEffect.SetStatusEffectCounters.SetCounters(counters);
+        }
+    }
+}
