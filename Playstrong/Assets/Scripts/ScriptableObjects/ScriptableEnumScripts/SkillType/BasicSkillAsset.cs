6819f6860cd42981e3966cc7327624244f81c050
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-10-26 15:20:47 +0800

    SkillCooldown Type Update

--- Playstrong/Assets/Scripts/ScriptableObjects/ScriptableEnumScripts/SkillType/BasicSkillAsset.cs
+++ Playstrong/Assets/Scripts/ScriptableObjects/ScriptableEnumScripts/SkillType/BasicSkillAsset.cs
@@ -19,13 +19,15 @@ namespace ScriptableObjects.Enums.SkillType
         public override void ReduceSkillCd(ISkill skill, int counter)
         {
             var logicTree = skill.CoroutineTreesAsset.MainLogicTree;
-            logicTree.AddCurrent(skill.SkillLogic.UpdateSkillCooldown.TurnReduceCooldown(counter));
+            
+            //logicTree.AddCurrent(skill.SkillLogic.UpdateSkillCooldown.TurnReduceCooldown(counter));
         }
         
         public override void ResetSkillCd(ISkill skill)
         {
             var logicTree = skill.CoroutineTreesAsset.MainLogicTree;
-            logicTree.AddCurrent(skill.SkillLogic.UpdateSkillCooldown.TurnResetCooldownToMax());
+            
+            //logicTree.AddCurrent(skill.SkillLogic.UpdateSkillCooldown.TurnResetCooldownToMax());
         }
         
         public override IEnumerator SetSkillReady(ISkillLogic skillLogic)
@@ -61,7 +63,7 @@ namespace ScriptableObjects.Enums.SkillType
         {
             var logicTree = skillLogic.Skill.CoroutineTreesAsset.MainLogicTree;
             
-            logicTree.AddCurrent(skillLogic.UpdateSkillCooldown.SetSkillCdToValue(counter));
+            //logicTree.AddCurrent(skillLogic.UpdateSkillCooldown.SetSkillCdToValue(counter));
 
             logicTree.EndSequence();
             yield return null;
