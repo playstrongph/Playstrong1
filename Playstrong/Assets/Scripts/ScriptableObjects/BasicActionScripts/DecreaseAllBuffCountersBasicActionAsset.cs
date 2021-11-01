30f878a1acf6d5cf5a834fa5505f1c9bba760981
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-11-01 19:29:06 +0800

    Cerise Complete

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActionScripts/DecreaseAllBuffCountersBasicActionAsset.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using References;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using UnityEngine;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "DecreaseAllBuffCounters", menuName = "SO's/BasicActions/DecreaseAllBuffCounters")]
+    
+    public class DecreaseAllBuffCountersBasicActionAsset : BasicActionAsset
+    {
+        [SerializeField] private int counters;
+
+        public override IEnumerator TargetAction(IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            var buffs = targetHero.HeroStatusEffects.HeroBuffEffects.HeroBuffs;
+
+            foreach (var buff in buffs)
+            {
+                buff.DecreaseStatusEffectCounters.DecreaseCounters(counters);
+            }
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator TargetAction(IHero thisHero,IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+
+            var buffs = targetHero.HeroStatusEffects.HeroBuffEffects.HeroBuffs;
+
+            foreach (var buff in buffs)
+            {
+                buff.DecreaseStatusEffectCounters.DecreaseCounters(counters);
+            }
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+       
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
