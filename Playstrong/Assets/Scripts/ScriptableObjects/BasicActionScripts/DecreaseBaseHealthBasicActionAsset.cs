f2750af64f6cc55815c5fac785ca7ee68ee1615a
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-11-03 14:13:49 +0800

    Advent Mortelix

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/BasicActionScripts/DecreaseBaseHealthBasicActionAsset.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using Interfaces;
+using Logic;
+using ScriptableObjects.SkillActionsScripts.BaseClassScripts;
+using UnityEngine;
+
+namespace ScriptableObjects.SkillActionsScripts
+{
+    [CreateAssetMenu(fileName = "DecreaseBaseHealth", menuName = "SO's/BasicActions/DecreaseBaseHealth")]
+    
+    public class DecreaseBaseHealthBasicActionAsset : BasicActionAsset
+    {
+        [SerializeField] private int flatHealth;
+        [SerializeField] private int percentHealth;
+
+        private int _changeHealthValue;
+
+        public override IEnumerator TargetAction(IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+            var currentHealth = targetHero.HeroLogic.HeroAttributes.Health;
+            
+            SetChangeHealthValue(targetHero);
+
+            var newBaseHealthValue = targetHero.HeroLogic.HeroAttributes.BaseHealth - _changeHealthValue;
+            newBaseHealthValue = Mathf.Max(newBaseHealthValue, 0);
+            
+            if(currentHealth > newBaseHealthValue)
+                targetHero.HeroLogic.SetHeroHealth.SetHealth(newBaseHealthValue);
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator TargetAction(IHero thisHero, IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTreesAsset.MainLogicTree;
+            var currentHealth = targetHero.HeroLogic.HeroAttributes.Health;
+            
+            SetChangeHealthValue(targetHero);
+
+            var newBaseHealthValue = targetHero.HeroLogic.HeroAttributes.BaseHealth - _changeHealthValue;
+            newBaseHealthValue = Mathf.Max(newBaseHealthValue, 0);
+            
+            if(currentHealth > newBaseHealthValue)
+                targetHero.HeroLogic.SetHeroHealth.SetHealth(newBaseHealthValue);
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+        private void SetChangeHealthValue(IHero hero)
+        {
+            var baseHealth = hero.HeroLogic.HeroAttributes.BaseHealth;
+            _changeHealthValue = Mathf.FloorToInt((float)baseHealth * percentHealth/100) + flatHealth;
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
