ecabf2029e130341eb8025d0759e366f12052c3b
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-11-03 13:18:16 +0800

    Rage Buff

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/StatusEffects/BuffEffectScripts/RageAsset.cs
@@ -0,0 +1,19 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace ScriptableObjects.StatusEffects.DebuffEffect
+{
+    [CreateAssetMenu(fileName = "Rage", menuName = "SO's/Status Effects/UniqueEffects/Rage")]
+    public class RageAsset : StatusEffectAsset
+    {
+        public override void ApplyStatusEffect(IHero hero) 
+        {
+            base.ApplyStatusEffect(hero);
+        }
+        
+        public override void UnapplyStatusEffect(IHero hero)
+        {
+            base.UnapplyStatusEffect(hero);
+        }
+    }
+}
