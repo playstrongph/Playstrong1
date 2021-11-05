71162f730e40be75e0c71248e34ed19354d7bc28
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-11-05 15:15:27 +0800

    Chief Is on the scene

---
+++ Playstrong/Assets/Scripts/ScriptableObjects/CalculatedDamageFactorValue/FixedMultiplierCalculatedFactorAsset.cs
@@ -0,0 +1,22 @@
+﻿using Interfaces;
+using References;
+using UnityEngine;
+
+namespace ScriptableObjects.DamageAttributeMultiple
+{
+    /// <summary>
+    /// Asset used by DealDamage basic action to deal percent damage according to damage taken by the hero
+    /// </summary>
+    [CreateAssetMenu(fileName = "FixedMultiplierFactor", menuName = "SO's/Scriptable Enums/CalculatedFactorValue/FixedMultiplierFactor")]
+    public class FixedMultiplierCalculatedFactorAsset : CalculatedFactorValueAsset
+    {
+       
+        
+        public override float GetCalculatedValue()
+        {
+            var multiplier = percentFactor / 100f; 
+           return multiplier;
+        }
+       
+    }
+}
